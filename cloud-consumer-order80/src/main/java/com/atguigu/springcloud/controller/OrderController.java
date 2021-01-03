8d53042e963f631717f9c3998cde5ed794f382fe
felix <1152062134@qq.com>
2021-01-03 18:14:40 +0800

    eurake集群化部署

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
@@ -17,7 +18,7 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
-    public static final String BASE_URL = "http://localhost:8001/payment/";
+    public static final String BASE_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @PostMapping(value = "consumer/payment/create")
     @ResponseBody
@@ -26,7 +27,7 @@ public class OrderController {
         CommonResult<Payment> commonResult = new CommonResult<Payment>();
         try{
             log.info("消息：消费者调创建订单接口"+payment.toString());
-            commonResult = restTemplate.postForObject(BASE_URL+"create",payment,CommonResult.class);
+            commonResult = restTemplate.postForObject(BASE_URL+"/payment/create",payment,CommonResult.class);
         }catch(Exception e){
             log.error("异常：消费者创建订单出现异常！");
             e.printStackTrace();
@@ -41,7 +42,7 @@ public class OrderController {
         CommonResult<Payment> commonResult = new CommonResult<Payment>();
         try {
             log.info("消息：消费者开始查询订单编号为："+id+"的信息");
-            commonResult = restTemplate.getForObject(BASE_URL+"get/"+id,CommonResult.class);
+            commonResult = restTemplate.getForObject(BASE_URL+"/payment/get/"+id,CommonResult.class);
         } catch (RestClientException e) {
             log.error("异常：消费者请求获取订单信息失败");
             e.printStackTrace();
