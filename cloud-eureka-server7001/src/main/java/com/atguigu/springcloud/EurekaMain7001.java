d20ecd0013d768914970e0620bf2e129d977aaa1
felix <1152062134@qq.com>
2021-01-03 11:03:38 +0800

    eureke单机版

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,12 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class Paymentmain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(Paymentmain8001.class);
+        SpringApplication.run(EurekaMain7001.class);
     }
 }
