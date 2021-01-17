1c006b541e8e5c98cfadb7d39a85d50a653ca733
felix <1152062134@qq.com>
2021-01-17 12:48:15 +0800

    完成stream学习

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.impl.MessageProviderImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+    @Resource
+    private MessageProviderImpl messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage(){
+        return "发送消息内容："+messageProvider.send()+"   发送成功！";
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@EnableBinding(Source.class)//定义消息推送管道
+@Slf4j
+public class MessageProviderImpl implements IMessageProvider {
+
+    @Resource
+    private MessageChannel output; // 消息发送管道
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        log.info("****产生随机序列号为："+serial);
+        return serial;
+    }
+}
